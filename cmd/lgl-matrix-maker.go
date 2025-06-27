17fd8f82140de67a420306909a5c64ac659711c0
Jens Brimforce <jens.brimberg@gmail.com>
2025-06-27 09:21:08 +0200

    Update lgl-matrix-maker.go

--- cmd/lgl-matrix-maker.go
+++ cmd/lgl-matrix-maker.go
@@ -2,61 +2,61 @@ package main
 
 import "fmt"
 
-struct LaserArea {
-  int height,
-  int width
+type LaserArea struct {
+  Height int16
+  Width int16
 }
 
-struct Layer {
-    string "id", // : "64ac3715-4f49-4f8b-a1d6-9ca169d7d080",
-    string "name", // : "03",
-    string "type", // : "Line",
-    bool "isOutputEnabled", // : true,
-    bool "isAirEnable", // : false,
-    int "height", // : 100,
-    int "width", // : 100,
-    string "color", // : "#00e000",
-    int "laserPower", // : 10,
-    int "laserSpeed", // : 1000,
-    float "engraveQuality", // : 0.091,
-    int "engraveCount", // : 1,
-    string "mode", // : "Fill",
-    int "sort", // : 0,
-    string "processMethod", // : "Engrave",
-    string "laserType", // : "",
-    int "materialId", // : "-1",
-    string "material", // : "",
-    int "thickness", // : 0,
-    bool "isCrossed", // : false,
-    bool "isOverscan", // : false,
-    float "overScanPer", // : 2.5,
-    string "imageMode", // : "Stucki",
-    float "scanAngle", // : 0,
-    bool "isLocked", // : false,
-    int "kerfOffset", // : 0,
-    bool "isBidirectional", // : true
+type Layer struct {
+    Id string // : "64ac3715-4f49-4f8b-a1d6-9ca169d7d080",
+    Name string // : "03",
+    Type string // : "Line",
+    IsOutputEnabled bool // : true,
+    IsAirEnable bool // : false,
+    Height int16 // : 100,
+    Width int16 // : 100,
+    Color string // : "#00e000",
+    LaserPower int16 // : 10,
+    LaserSpeed int16 // : 1000,
+    EngraveQuality float32 // : 0.091,
+    EngraveCount int16 // : 1,
+    Mode string // : "Fill",
+    Sort int16 // : 0,
+    ProcessMethod string // : "Engrave",
+    LaserType string // : "",
+    MaterialId int16 // : "-1",
+    Material string // : "",
+    Thickness int16 // : 0,
+    IsCrossed bool // : false,
+    IsOverscan bool // : false,
+    OverScanPer float32 // : 2.5,
+    ImageMode string // : "Stucki",
+    ScanAngle float32 // : 0,
+    IsLocked bool // : false,
+    KerfOffset int16 // : 0,
+    IsBidirectional bool // : true
 }
 
-struct Element {
-    string "id", //uuid
-    string "type", // : "shape-rect",
-    int "angle", //: 0,
-    bool "selectable", // : true,
-    bool "visible", // : true,
-    string "layerId", // : "64ac3715-4f49-4f8b-a1d6-9ca169d7d080",
-    bool "isLocked", // : false,
-    bool "isOutWorkSpace", // : false,
-    string "lineColor", // : "#00e000",
-    int "width", // : 5,
-    int "height", // : 6,
-    int[] transformMatrix, // [1,0,0,1,x,y]
+type Element struct {
+    Id string //uuid
+    Type string // : "shape-rect",
+    Angle int16 //: 0,
+    Selectable bool // : true,
+    Visible bool // : true,
+    LayerId string // : "64ac3715-4f49-4f8b-a1d6-9ca169d7d080",
+    IsLocked bool // : false,
+    IsOutWorkSpace bool // : false,
+    LineColor string // : "#00e000",
+    Width int16 // : 5,
+    Height int16 // : 6,
+    TransformMatrix []int16 // [1,0,0,1,x,y]
 }
 
-struct LglStruct {
-    string version, //"1.2.1",
-    LaserArea LaserArea,
-    Layer[] Layers,
-    Element[] Elements,
+type LglStruct struct {
+    Version string //"1.2.1",
+    LaserArea LaserArea
+    Layers []Layer
+    Elements []Element
 }
 
 func main() {
